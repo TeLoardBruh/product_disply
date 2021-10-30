2ee6c7a264784f26dd1598584d0648913349283a
sovannmonyreak hing <damnloardbruh@gmail.com>
2021-10-30 17:14:13 +0700

    change style

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <carousel />
-    <div class="pt-5 pb-5 blue lighten-2 text-center white--text">
+    <div class="pt-5 pb-5 black lighten-2 text-center white--text">
       <span><v-icon class="mb-2" color="white">mdi-home</v-icon></span>
       <span>Phnom Penh (រាជធានី​ភ្នំពេញ), Cambodia</span>
     </div>
